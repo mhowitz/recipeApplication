3ddcb88dde4e25936c9ba13e2e80b0f3a5626c8f
mikwitz <mikaylahowitz@gmail.com>
2021-12-02 13:50:21 -0700

    console testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 
+//Fetch Recipes from Edamam using ingredients
+
 var getRecipes = function(foodItem) {
     var recipeUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${foodItem}&app_id=fbfe1471&app_key=470a241a4057e8b1f7fe899f21000837`;
     fetch(recipeUrl).then(function(response){
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                recipeData = data.hits;
+                console.log(recipeData);
+                console.log(recipeData[0].recipe.label)
+                console.log(recipeData[0].recipe.ingredientLines);
+
             }
             )} else {
                 console.log("data not found")
@@ -14,8 +21,26 @@ var getRecipes = function(foodItem) {
 
 getRecipes("chicken", "egg");
 
+//client_id (Kroger) : recipesearchapp-8c839beb3d4b9472a15caa209e06ecb34253613186576661838    &client_id=recipesearchapp-8c839beb3d4b9472a15caa209e06ecb34253613186576661838
+//client_secret (Kroger): 9q6cx5dUado52MQ4McyzkQ2ptPCG-PsbUDWUI6lw  &client_secret=9q6cx5dUado52MQ4McyzkQ2ptPCG-PsbUDWUI6lw
+
+//HAVING TROUBLE WITH KROGER API- NEED SOME SORT OF AUTHORIZATION I CANNOT FIGURE OUT HOW TO DO
 
 
+// var getIngredients = function(ingredient) {
+//     var ingredientsUrl = `https://api.kroger.com/v1/products?filter.term=${ingredient}`
+//     fetch(ingredientsUrl).then(function(response){
+//         if(response.ok) {
+//             response.json().then(function(data) {
+//                 console.log(data);
+//             })
+//         } else {
+//             console.log("grocery data not found");
+//         }
+//     })
+// }
+
+// getIngredients("bread");
 
 
 //when searching an ingredient, approx 20 recipes are displayed in a array called 'hits'
