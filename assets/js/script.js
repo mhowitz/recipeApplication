886bf68c7aa29b58946f956d74655870c7ea6244
mikwitz <mikaylahowitz@gmail.com>
2021-12-03 12:23:45 -0700

    nothing changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ var getRecipes = function(foodItem) {
                 console.log(recipeData);
                 console.log(recipeData[0].recipe.label)
                 console.log(recipeData[0].recipe.ingredientLines);
+                console.log(recipeData[0].recipe.url)
 
             }
             )} else {
@@ -19,28 +20,9 @@ var getRecipes = function(foodItem) {
     })
 };
 
-getRecipes("chicken", "egg");
-
-//client_id (Kroger) : recipesearchapp-8c839beb3d4b9472a15caa209e06ecb34253613186576661838    &client_id=recipesearchapp-8c839beb3d4b9472a15caa209e06ecb34253613186576661838
-//client_secret (Kroger): 9q6cx5dUado52MQ4McyzkQ2ptPCG-PsbUDWUI6lw  &client_secret=9q6cx5dUado52MQ4McyzkQ2ptPCG-PsbUDWUI6lw
-
-//HAVING TROUBLE WITH KROGER API- NEED SOME SORT OF AUTHORIZATION I CANNOT FIGURE OUT HOW TO DO
+getRecipes("chicken kale");
 
 
-// var getIngredients = function(ingredient) {
-//     var ingredientsUrl = `https://api.kroger.com/v1/products?filter.term=${ingredient}`
-//     fetch(ingredientsUrl).then(function(response){
-//         if(response.ok) {
-//             response.json().then(function(data) {
-//                 console.log(data);
-//             })
-//         } else {
-//             console.log("grocery data not found");
-//         }
-//     })
-// }
-
-// getIngredients("bread");
 
 
 //when searching an ingredient, approx 20 recipes are displayed in a array called 'hits'
