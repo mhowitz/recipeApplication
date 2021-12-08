c5a172519589c1a9acf1478223000a7c95177532
Chelseao86 <92495487+Chelseao86@users.noreply.github.com>
2021-12-07 19:18:01 -0700

    tues edits before class 211207

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,23 +1,24 @@
-
-
-
-//  Determine current location       
-function showPosition(position) {
-  var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-  var img_url = "https://api.mapbox.com/styles/v1/chelseao86/ckwv8wnqv0vww15oe26f2wa7c/draft.html?
-	title=false&access_token=pk.eyJ1IjoiY2hlbHNlYW84NiIsImEiOiJja3d2OGlpN3EwM2Y1MnZzZXJ6NGhubmkyIn0.3AK1uWnQqg3Mq8Y2JJE0HQ&zoomwheel=false#12/48.8665/2.3176" title="Basic" 
-	style="border:none;";
-
-  document.getElementById(“iframe”).innerHTML = "<img src='"+img_url+"'>";
-}
-    
-
-  
-  // Send a map request using  Type of “grocery” and Proximity equal to HTML position function
-  
-  $ curl "https://api.mapbox.com/styles/v1/chelseao86/ckwv8wnqv0vww15oe26f2wa7c/draft.html?
-      title=false&access_token=pk.eyJ1IjoiY2hlbHNlYW84NiIsImEiOiJja3d2OGlpN3EwM2Y1MnZzZXJ6NGhubmkyIn0.3AK1uWnQqg3Mq8Y2JJE0HQ&zoomwheel=false#12/48.8665/2.3176" title="Basic" 
-      style="border:none;"
-  
-  
+const x = document.getElementById("location");
+        
+        function getLocation() {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        }
+        
+        function showPosition(position) {
+          x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+        };
+       
+       
+        mapboxgl.accessToken = 'pk.eyJ1IjoiY2hlbHNlYW84NiIsImEiOiJja3d2OGlpN3EwM2Y1MnZzZXJ6NGhubmkyIn0.3AK1uWnQqg3Mq8Y2JJE0HQ';
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-111.8910474, 40.7607793], // starting position [lng, lat]
+            zoom: 9 // starting zoom
+        });
+       
